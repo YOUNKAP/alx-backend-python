428f16dd6f3b93812877ecca62bc6ee56481ae2a
YOUNKAP <nina.younkap@ccaa.pro>
2024-03-15 20:52:30 -0400

    mypy compliant

--- 0-add.py
+++ 0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-
-'''A python module that returns the summation of two float numbers'''
+"""
+Basic annotations - add
+"""
 
 
 def add(a: float, b: float) -> float:
-    '''A function that returns the summation of two float numbers'''
+    """
+    function to add two floats
+    """
     return a + b
